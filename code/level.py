06142eb1da0e5cabeebff197137c4dacd4af7989
Barba2k2 <lorenzo.dz@hotmail.com>
2023-05-11 10:04:24 -0300

    Adciocado colisões

--- code/level.py
+++ code/level.py
@@ -14,6 +14,7 @@ class Level:
 
             # sprite groups
             self.all_sprites = CameraGroup()
+            self.collision_sprites = pygame.sprite.Group()
             
             self.setup()
             self.overlay = Overlay(self.player)
@@ -32,7 +33,7 @@ class Level:
                 
             # Fence
             for x, y, surf in tmx_data.get_layer_by_name('Fence').tiles():
-                Generic((x * TILE_SIZE, y * TILE_SIZE), surf, self.all_sprites)
+                Generic((x * TILE_SIZE, y * TILE_SIZE), surf, [self.all_sprites, self.collision_sprites])
             
             # Water
             water_frames = import_folder('G:/Meu Drive/Pygame/ZeldaGame/graphics/water/')
@@ -41,13 +42,20 @@ class Level:
             
             # Trees
             for obj in tmx_data.get_layer_by_name('Trees'):
-                Tree((obj.x, obj.y), obj.image, self.all_sprites, obj.name)
+                Tree((obj.x, obj.y), obj.image, [self.all_sprites, self.collision_sprites], obj.name)
             
             # WildFlowers
             for obj in tmx_data.get_layer_by_name('Decoration'):
-                WindFlower((obj.x, obj.y), obj.image, self.all_sprites)
+                WindFlower((obj.x, obj.y), obj.image, [self.all_sprites, self.collision_sprites])
             
-            self.player = Player((640, 360), self.all_sprites)
+            # Collision Tiles
+            for x, y, surf in tmx_data.get_layer_by_name('Collision').tiles():
+                Generic((x * TILE_SIZE, y * TILE_SIZE), pygame.Surface((TILE_SIZE, TILE_SIZE)), self.collision_sprites)
+            
+            # Player
+            for obj in tmx_data.get_layer_by_name('Player'):
+                if obj.name == 'Start':
+                    self.player = Player((obj.x, obj.y), self.all_sprites, self.collision_sprites)
             Generic(
                 pos = (0, 0),
                 surf = pygame.image.load('G:/Meu Drive/Pygame/ZeldaGame/graphics/world/ground.png').convert_alpha(),
--- code/player.py
+++ code/player.py
@@ -4,7 +4,7 @@ from support import *
 from timerr import Timer
 
 class Player(pygame.sprite.Sprite):
-	def __init__(self, pos, group):
+	def __init__(self, pos, group, collision_sprites):
 		super().__init__(group)
 
 		self.import_assets()
@@ -20,6 +20,10 @@ class Player(pygame.sprite.Sprite):
 		self.direction = pygame.math.Vector2()
 		self.pos = pygame.math.Vector2(self.rect.center)
 		self.speed = 200
+   
+		# Collision
+		self.hitbox = self.rect.copy().inflate((-126, -70))
+		self.collision_sprites = collision_sprites
 
 		# timers 
 		self.timers = {
@@ -44,18 +48,16 @@ class Player(pygame.sprite.Sprite):
 		# print(self.selected_tool)
 
 	def use_seed(self):
-
 		pass
 
 	def import_assets(self):
 		self.animations = {
-      		'up': [],'down': [],'left': [],'right': [],
+			'up': [],'down': [],'left': [],'right': [],
 			'right_idle':[],'left_idle':[],'up_idle':[],'down_idle':[],
 			'right_hoe':[],'left_hoe':[],'up_hoe':[],'down_hoe':[],
 			'right_axe':[],'left_axe':[],'up_axe':[],'down_axe':[],
 			'right_water':[],'left_water':[],'up_water':[],'down_water':[]
-   		}
-
+		}
 		for animation in self.animations.keys():
 			full_path = 'G:/Meu Drive/Pygame/ZeldaGame/graphics/character/' + animation
 			self.animations[animation] = import_folder(full_path)
@@ -64,7 +66,7 @@ class Player(pygame.sprite.Sprite):
 		self.frame_index += 4 * dt
 		if self.frame_index >= len(self.animations[self.status]):
 			self.frame_index = 0
-
+	
 		self.image = self.animations[self.status][int(self.frame_index)]
 
 	def input(self):
@@ -117,7 +119,7 @@ class Player(pygame.sprite.Sprite):
 				self.selected_seed = self.seeds[self.seed_index]
 
 	def get_status(self):
-		
+			
 		# idle
 		if self.direction.magnitude() == 0:
 			self.status = self.status.split('_')[0] + '_idle'
@@ -130,6 +132,27 @@ class Player(pygame.sprite.Sprite):
 		for timer in self.timers.values():
 			timer.update()
 
+	def collision(self, direction):
+		for sprite in self.collision_sprites.sprites():
+			if hasattr(sprite, 'hitbox'):
+				if sprite.hitbox.colliderect(self.hitbox):
+					if direction == 'horizontal':
+						if self.direction.x > 0: # moving right
+							self.hitbox.right = sprite.hitbox.left
+						if self.direction.x < 0: # moving left
+							self.hitbox.left = sprite.hitbox.right
+						self.rect.centerx = self.hitbox.centerx
+						self.pos.x = self.hitbox.centerx
+					
+					if direction == 'vertical':
+						if self.direction.y > 0: # moving down
+							self.hitbox.bottom = sprite.hitbox.top
+						if self.direction.y < 0: # moving up
+							self.hitbox.top = sprite.hitbox.bottom
+						self.rect.centery = self.hitbox.centery
+						self.pos.y = self.hitbox.centery
+							
+  
 	def move(self,dt):
 
 		# normalizing a vector 
@@ -138,11 +161,15 @@ class Player(pygame.sprite.Sprite):
 
 		# horizontal movement
 		self.pos.x += self.direction.x * self.speed * dt
-		self.rect.centerx = self.pos.x
+		self.hitbox.centerx = round(self.pos.x)
+		self.rect.centerx = self.hitbox.centerx
+		self.collision('horizontal')
 
 		# vertical movement
 		self.pos.y += self.direction.y * self.speed * dt
-		self.rect.centery = self.pos.y
+		self.hitbox.centery = round(self.pos.y)
+		self.rect.centery = self.hitbox.centery
+		self.collision('vertical')
 
 	def update(self, dt):
 		self.input()
