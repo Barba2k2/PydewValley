b8065dbb78eb00dc1b54b5dc03b772be8740476d
Barba2k2 <lorenzo.dz@hotmail.com>
2023-05-11 10:47:44 -0300

    Adicionado método para derrubar as arvores

--- code/sprites.py
+++ code/sprites.py
@@ -1,5 +1,8 @@
+from typing import Any
 import pygame
 from settings import *
+from random import randint, choice
+from timerr import Timer
 
 class Generic(pygame.sprite.Sprite):
     def __init__(self, pos, surf, groups, z = LAYERS['main']):
@@ -42,4 +45,46 @@ class Tree(Generic):
     def __init__(self, pos, surf, groups, name):
         super().__init__(pos, surf, groups)
         
+        # Tree Atributes
+        self.health = 5
+        self.alive = True
+        stump_path = f'G:/Meu Drive/Pygame/ZeldaGame/graphics/stumps/{"small" if name == "Small" else "large"}.png'
+        self.stump_surf = pygame.image.load(stump_path).convert_alpha()
+        self.invul_timer = Timer(200)
+        # Apples
+        self.apple_surf = pygame.image.load('G:/Meu Drive/Pygame/ZeldaGame/graphics/fruit/apple.png')
+        self.apple_pos = APPLE_POS[name]
+        self.apple_sprites = pygame.sprite.Group()
+        self.create_fruit()
+    
+    def damage(self):
+        # damaging the tree
+        self.health -= 1
         
+        # remove an apple
+        if len(self.apple_sprites.sprites()) > 0:
+            random_apple = choice(self.apple_sprites.sprites())
+            random_apple.kill()
+            
+    def check_death(self):
+        if self.health <= 0:
+            self.image = self.stump_surf
+            self.rect = self.image.get_rect(midbottom = self.rect.midbottom)
+            self.hitbox = self.hitbox.copy().inflate(-10, -self.rect.height * 0.6)
+            self.alive = False
+        
+    def update(self):
+        if self.alive:
+            self.check_death()
+     
+    def create_fruit(self):
+        for pos in self.apple_pos:
+            if randint(0, 10) < 2:
+                x = pos[0] + self.rect.left
+                y = pos[1] + self.rect.top
+                Generic(
+                    pos = (x, y), 
+                    surf = self.apple_surf, 
+                    groups = [self.apple_sprites, self.groups()[0]],
+                    z = LAYERS['fruit']
+                )
