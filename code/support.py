9e63b221c36787ac47f2ed62d052f3b0e10b139e
Barba2k2 <lorenzo.dz@hotmail.com>
2023-05-12 10:01:39 -0300

    Logica para juntar as sprites do solo

--- code/support.py
+++ code/support.py
@@ -11,3 +11,13 @@ def import_folder(path):
             surface_list.append(image_surf)
     
     return surface_list
+
+def import_folder_dict(path):
+    surface_dict = {}
+    
+    for _, __, img_files in walk(path):
+        for image in img_files:
+            image_surf = path + '/' + image
+            surface_dict[image.split('.')[0]] = image_surf
+    
+    return surface_dict
